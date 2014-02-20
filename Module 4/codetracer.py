81a53e25dfaebe733f267a6afdc761a57ebe4561
Mark Dehus <mark.dehus@colorado.edu>
2014-02-20 11:45:43 -0700

    Added simple decorator example for tracing code

---
+++ Module 4/codetracer.py
@@ -0,0 +1,10 @@
+import sys
+
+def logger(func):
+    def inner(*args, **kwargs):
+        msg = "{1}.{2}(".format(args[0], func.__name__)
+        msg += ', '.join([str(arg) for arg in args[1:]])
+        msg += ")\n"
+        sys.stderr.write(msg)
+        return func(*args, **kwargs)
+    return inner
