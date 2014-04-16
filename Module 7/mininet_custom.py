9719589b64971f87fc9cd1007c0d56c4b7e6483d
Mark Dehus <mark.dehus@colorado.edu>
2014-04-16 13:04:10 -0600

    simplified custom mininet example

--- Module 7/mininet_custom.py
+++ Module 7/mininet_custom.py
@@ -16,26 +16,15 @@ def allfour():
     info( '*** Adding hosts\n' )
     h1 = net.addHost( 'h1', ip='10.0.0.1' )
     h2 = net.addHost( 'h2', ip='10.0.0.2' )
-    h3 = net.addHost( 'h3', ip='10.0.0.3' )
-    h4 = net.addHost( 'h4', ip='10.0.0.4' )
 
     info( '*** Adding switch\n' )
     s1 = net.addSwitch( 's1' )
     s2 = net.addSwitch( 's2' )
-    s3 = net.addSwitch( 's3' )
-    s4 = net.addSwitch( 's4' )
 
     info( '*** Creating links\n' )
     net.addLink( h1, s1)
     net.addLink( h2, s2 )
-    net.addLink( h3, s3 )
-    net.addLink( h4, s4 )
     net.addLink( s1, s2 )
-    net.addLink( s2, s3 )
-    net.addLink( s3, s4 )
-    #net.addLink( s4, s1 )
-    
-    
 
     info( '*** Starting network\n')
     net.start()
