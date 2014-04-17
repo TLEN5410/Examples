9a01019a9991d5c72a13c6601d7dd5e33ecf86c9
Mark Dehus <mark.dehus@colorado.edu>
2014-04-17 00:06:00 -0600

    fixed src / dst issue

--- Module 7/simplecomponents.py
+++ Module 7/simplecomponents.py
@@ -34,26 +34,11 @@ class SimpleSwitch(object):
         self.mac_table = {}
         core.openflow.addListeners(self)
 
-    def _forward(self, event, packet):
-        ''' Instructs the switch that raised the event to forward 
-            directly to the port we know that the destiniation mac
-            is on.  If the lookup for the destination mac fails, 
-            KeyError will be raised.
-
-            Arugments:
-                event: The event passed in from POX.
-                packet: The packet associated with the event 
-                        that occurred.
-
-            Raises:
-                KeyError: If the event.connection, packet.dst tuple 
-                          doesn't exist in our mac table.
-        '''
-        destination_port = self.mac_table[(event.connection, packet.src)]
+    def forward(self, event, packet):
+        destination_port = self.mac_table[(event.connection, packet.dst)]
 
         # Create a flow rule in the direction from sender to recipient
         # that maps to the port it came in on.
-        self.logger.debug("Installing %s <-> %s" % (packet.src, packet.dst))
         message = of.ofp_flow_mod()
         message.match.dl_dst = packet.src
         message.match.dl_src = packet.dst
@@ -68,29 +53,40 @@ class SimpleSwitch(object):
         message.match.dl_dst = packet.dst
         message.actions.append(of.ofp_action_output(port=destination_port))
         event.connection.send(message)
+        self.logger.info("Installing %s <-> %s" % (packet.src, packet.dst))
 
     def _handle_PacketIn(self, event):
         packet = event.parsed
-        self.logger.info("Got a packet from %s to %s",
-                                 packet.src, packet.dst)
-
+        self.logger.info("Got a packet from %s to %s", 
+                         packet.src, packet.dst)
+        
         # Learn the source
         self.mac_table[(event.connection, packet.src)] = event.port
-        self.logger.info("Added (%s, %s) at %s to the mac table",
+        self.logger.info("Added (%s, %s) at %s to the mac table", 
                          event.connection, packet.src, event.port)
 
         try:
             self.logger.info("Trying to forward")
-            self._forward(event, packet)
+            self.forward(event, packet)
         except KeyError:
             self.logger.info("Broadcasting instead")
             # We don't know where the recipient is yet
             self.broadcast(event)
 
+    def broadcast(self, event):
+        ''' Instructs the switch that caused the event to be raised to simply
+            broadcast the received packet out on all ports, execpt the one it
+            came in on.
+
+            Arguments:
+                event: The event passed in from POX.
+            
+        '''
+        message = of.ofp_packet_out(data=event.ofp)
+        message.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        event.connection.send(message)
+
 
 def launch():
-    # Pick one or the other here, but not both
-    core.registerNew(SimpleHub)
-    #core.registerNew(SimpleSwitch)
-
-
+    #core.registerNew(SimpleHub)
+    core.registerNew(SimpleSwitch)
